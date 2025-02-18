c19cfe7749a5f771a5e87125f00abaa06b1ac283
Joshua Buchsbaum <143635502+jbuchsbaum03@users.noreply.github.com>
2025-02-18 14:24:11 -0500

    Update OLED_GIF.py

--- OLED_GIF.py
+++ OLED_GIF.py
@@ -17,7 +17,7 @@ from threading import Thread
 
 class OLED_GIF:
     def __init__(self):
-        corePropsPath = r"C:\SteelSeries Replacement\Data\SteelSeries\GG\coreProps.json"
+        corePropsPath = r"C:\ProgramData\SteelSeries\GG\coreProps.json"
         self.sseAddress = f'http://{json.load(open(corePropsPath))["address"]}'
         self.game = "OLED_GIF"
         self.game_display_name = 'Display OLED GIF'
